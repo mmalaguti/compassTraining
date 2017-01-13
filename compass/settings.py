56fe6abd2ac8381e586ec544e97e9eec5cc4a087
mSquare <marinamalaguti@gmail.com>
2017-01-12 18:15:00 -0600

    website

--- compass/settings.py
+++ compass/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #print(BASE_DIR)
-PROJECT_ROOT= os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'compass.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_ROOT, 'static/templates'),],
+        'DIRS': [os.path.join(PROJECT_ROOT,'compassTraining/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static-only')
+STATIC_URL = '/root/static/'
+#STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = "/static/media/"
